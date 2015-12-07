80fa1b199fe95a9a3b744d5dde9f59f31845469e
Han Yu <hyu@C02PT12JG8WM.group.on>
2015-12-07 15:42:00 -0800

    Initial commit

---
+++ src/helloworld.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by hyu on 12/2/15.
+  */
+
+import org.apache.spark.sql._
+import scala.util.matching._
+import java.util._
+import java.text.SimpleDateFormat
+import java.sql.Timestamp
+import java.io._
+
+class helloworld {
+  def hello(): Unit ={
+    println("hello world")
+  }
+}
